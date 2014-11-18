4961752727f0d4cbc6f60b3c0e10979359245aba
Mickael <mickx971.l@gmail.com>
2014-11-18 22:40:35 +0100

    Connection listener <--> gui

--- src/core/play/MoveEventListener.java
+++ src/core/play/MoveEventListener.java
@@ -1,11 +1,27 @@
 package core.play;
 
+import gui.GUI;
+
 import java.util.EventListener;
 
 public class MoveEventListener implements EventListener {
 	
+	private GUI gui;
+	private static MoveEventListener listener;
+	
+	private MoveEventListener(GUI gui) {
+		this.gui = gui;
+	}
+	
+	public static MoveEventListener getListener() {
+		if( listener == null ) {
+			listener = new MoveEventListener(GUI.getGUI());
+		}
+		return listener;
+	}
+	
 	public void onEvent(MoveEventData data) {
-		System.out.println("coucou");
+		gui.refresh(data);
 	}
 	
 }
